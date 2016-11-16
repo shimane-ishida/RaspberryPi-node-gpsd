09173c759768ed5fe57fe46b93a402624ce14794
shimane-ishida <simane.isida@gmail.com>
2016-11-16 16:21:32 +0900

    update map.js

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,5 +1,5 @@
 $(function (){
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([35.384069, 132.764915], 13);
 
     L.tileLayer('tiles/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
