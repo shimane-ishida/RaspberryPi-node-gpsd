d86566916d78121e92fa09a1fc5c7d268160d53e
shimane-ishida <simane.isida@gmail.com>
2016-11-16 15:54:00 +0900

    update map.js

---
+++ public/javascripts/map.js
@@ -0,0 +1,12 @@
+$(function (){
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('tiles/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([51.5, -0.09]).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+
+});
