de657ad85183a908e570dae769060864772e5197
shimane-ishida <simane.isida@gmail.com>
2016-11-16 18:05:11 +0900

    update

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,4 +1,3 @@
-var map;
 $(function (){
     map = L.map('map').setView([35.384069, 132.764915], 12);
 
@@ -8,8 +7,35 @@ $(function (){
 });
 
 function onCreateMapMarker(LatLon){
+    if (_this.marker_list.length != 0) {
+        // マーカーのクリア
+        _this.marker_list.forEach(function(gmaker, idx) {
+        });
+    }
     var date = new Date(LatLon.time);
-    L.marker([LatLon.lat, LatLon.lon]).addTo(map)
-        .bindPopup('更新時間:'+date.toLocaleString())
-        .openPopup();
+
+    var marker = new ol.Feature({
+        geometry: new ol.geom.Point(ol.proj.fromLonLat([LatLon.lat, LatLon.lon])),
+        name: '更新時間:'+date.toLocaleString(), // 車両名
+    });
+    var vectorLayer = new ol.layer.Vector({
+        source: new ol.source.Vector({
+            features: [marker],
+            attributions: ['Icon desigined by <a href="http://www.flaticon.com/free-icon/snowplow_226399">Madebyoliver from Flaticon</a>'],
+        }),
+        style: new ol.style.Style({
+            image: new ol.style.Icon({
+                anchor: [0.5, 1], // 基準をアイコンの左右真ん中・下端とする
+            })
+        }),
+        mail: "",
+        type: 'marker'
+
+    });
+    this.map.addLayer(vectorLayer);
+    this.marker_list.push(marker);
+
+    // var map = L.marker([LatLon.lat, LatLon.lon]).addTo(map)
+    //     .bindPopup('更新時間:'+date.toLocaleString())
+    //     .openPopup();
 }
