f76aa1d22eeb4a1d32196e6a65ed832f11142b29
shimane-ishida <simane.isida@gmail.com>
2016-11-16 14:38:34 +0900

    test

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -3,12 +3,19 @@ var socket = io.connect();
 // WebSocketでの接続
 socket.on('connect', function(msg) {
     console.log("connect");
-    $('#connectId').html('あなたの接続ID::' + socket.socket.transport.sessid);
+    $('#connectId').html('接続ID::' + socket.socket.transport.sessid);
     $('#type').html('接続方式::' + socket.socket.transport.name);
 });
 
 // メッセージを受けたとき
 socket.on('message', function(msg) {
     // メッセージを画面に表示する
-    $('#receiveMsg').prepend(msg.value + '<br>');
+    var obj = msg.value;
+    $('#receiveMsg').prepend(obj.lat + '<br>');
 });
+
+var pase = function(args){
+    var res = Object;
+
+    return res;
+}
