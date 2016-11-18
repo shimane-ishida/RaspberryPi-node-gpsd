9d9f547051223e0bc6b22d420aca9ae89dbce11c
shimane-ishida <simane.isida@gmail.com>
2016-11-18 09:21:07 +0900

    update

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,18 +1,32 @@
 $(function (){
-    map = L.map('map').setView([35.384069, 132.764915], 16);
-
-//    L.tileLayer('/tiles/{z}/{x}/{y}.png', {
-//        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-//    }).addTo(map);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    map = L.map('map').setView([35.384069, 132.764915], 17);
+    MarkerLayer = new L.layerGroup();
+    L.tileLayer('/tiles/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+//    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//    }).addTo(map);
 });
 
 function onCreateMapMarker(LatLon){
-
+      var markers = [];
+//    console.log(MarkerLayer.getLayers().length);
+    if(MarkerLayer.getLayers !=0 ){
+        MarkerLayer.clearLayers();
+    }
     var date = new Date(LatLon.time);
-    L.marker([LatLon.lat, LatLon.lon]).addTo(map)
-        .bindPopup('更新時間:'+date.toLocaleString())
-        .openPopup();
+    var marker = new L.Marker([LatLon.lat, LatLon.lon]);
+    var popup = L.popup({
+       closeButton: false,
+       closeOnClick: false
+    })
+        .setContent('<p>更新時間:'+date.toLocaleString()+'<p>')
+        .setLatLng([LatLon.lat,LatLon.lon]);
+    marker.bindPopup(popup);
+    markers.push(marker);
+    var markerlayer = new L.layerGroup(markers).addTo(this.map);
+    MarkerLayer = markerlayer;
+    map.panTo([LatLon.lat,LatLon.lon]);
+    
 }
--- public/javascripts/sockets.js
+++ public/javascripts/sockets.js
@@ -3,8 +3,6 @@ var socket = io.connect();
 // WebSocketでの接続
 socket.on('connect', function(msg) {
     console.log("connect");
-    $('#connectId').html('接続ID::' + socket.socket.transport.sessid);
-    $('#type').html('接続方式::' + socket.socket.transport.name);
 });
 
 // メッセージを受けたとき
